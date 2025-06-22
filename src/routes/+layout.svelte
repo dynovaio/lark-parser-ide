29db57ad3ca9d41e93d85e1302eb11c267c80018
zodiacfireworks <martin.vuelta@gmail.com>
2025-06-22 16:12:03 -0500

    Update: remove bulma css, use new Header component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,78 +1,13 @@
 <script lang="ts">
-  import 'bulma/css/bulma.css';
   import '../app.css';
 
   import Header from '$lib/components/Header.svelte';
 
-  import FullscreenIcon from '$lib/icons/FullScreen.svelte';
-  import ExitFullscreenIcon from '$lib/icons/ExitFullScreen.svelte';
-
-  let fullscreen = $state(false);
   let { children } = $props();
-
-  function openFullscreen() {
-    let elem = document.getElementsByTagName('body')[0];
-    elem.requestFullscreen();
-
-    fullscreen = true;
-  }
-
-  function closeFullscreen() {
-    document.exitFullscreen();
-    fullscreen = false;
-  }
 </script>
 
-<Header />
-<header class="page-header">
-  <nav class="navbar has-background-white" aria-label="main navigation">
-    <div class="navbar-brand">
-      <a class="navbar-item" href="/" title="Home">
-        <img src="lark-logo.png" alt="lark logo" />
-      </a>
-    </div>
-
-    <div class="navbar-menu">
-      <div class="navbar-end">
-        <div class="navbar-item">
-          <div class="buttons">
-            <a
-              class="button is-link"
-              href="https://lark-parser.readthedocs.io/en/latest/"
-              target="_blank"
-            >
-              Parser Docs
-            </a>
-            <a class="button is-link" href="https://github.com/lark-parser/lark" target="_blank">
-              Parser Source
-            </a>
-            <a class="button is-link" href="https://github.com/lark-parser/ide" target="_blank">
-              IDE Source
-            </a>
-            {#if fullscreen}
-              <button class="button is-normal is-light" onclick={closeFullscreen}>
-                <ExitFullscreenIcon />
-              </button>
-            {:else}
-              <button class="button is-normal is-light" onclick={openFullscreen}>
-                <FullscreenIcon />
-              </button>
-            {/if}
-          </div>
-        </div>
-      </div>
-    </div>
-  </nav>
-</header>
+<Header></Header>
 
 <main class="page-main">
   {@render children()}
 </main>
-
-<style>
-  nav {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-  }
-</style>
