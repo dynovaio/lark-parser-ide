27afbee8eecf15ba771a9a0a3a1c79d642fff247
zodiacfireworks <martin.vuelta@gmail.com>
2025-06-22 18:41:03 -0500

    Add: Loader component

---
+++ src/lib/components/Loader.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import LarkParserLogoSquare from '$lib/icons/LarkParserLogoSquare.svelte';
+  interface Props {
+    message: string;
+    progress: number;
+  }
+
+  const { message = $bindable('Loading...'), progress = $bindable(0) }: Props = $props();
+</script>
+
+<div
+  class="page-loader flex h-full w-full flex-col items-center justify-center space-y-4 text-gray-800"
+>
+  <LarkParserLogoSquare size={256} aria-label="Lark Parser" />
+  <div class="relative h-6 w-64 overflow-hidden rounded-full bg-black/40">
+    <div
+      class="h-full w-full bg-green-500 transition-transform duration-500 ease-in-out"
+      style={`transform: translateX(-${100 - (100 * (progress ?? 0)) / 100}%)`}
+    ></div>
+  </div>
+  <p class="">{message}</p>
+</div>
---
+++ src/stories/LarkLoader.stories.svelte
@@ -0,0 +1,24 @@
+<script module>
+  import '../app.css';
+
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+  import Loader from '$lib/components/Loader.svelte';
+
+  const { Story } = defineMeta({
+    title: 'Lark IDE/Loader',
+    component: Loader,
+    tags: ['autodocs'],
+    argTypes: {
+      message: {
+        control: { type: 'text', default: 'Loading...' },
+        description: 'Message to display while loading'
+      },
+      progress: {
+        control: { type: 'range', min: 0, max: 100, step: 1, default: 50 },
+        description: 'Progress percentage (0-100)'
+      }
+    }
+  });
+</script>
+
+<Story name="Default" args={{ message: 'Loading...', progress: 50 }} />
