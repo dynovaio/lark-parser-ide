f3e7860169a5f66811521dfac469ce8f4d4f1b3a
zodiacfireworks <martin.vuelta@gmail.com>
2025-06-24 01:37:40 -0500

    Update: rename toolbar component

--- src/lib/components/Ide.svelte
+++ src/lib/components/Ide.svelte
@@ -1,15 +1,27 @@
 <script lang="ts">
-  import Toolbar from '$lib/components/Ide/Toolbar.svelte';
+  import Projects from '$lib/components/Ide/Projects.svelte';
   import StatusBar from '$lib/components/Ide/StatusBar.svelte';
   import GrammarEditor from '$lib/components/Ide/GrammarEditor.svelte';
   import TestList from '$lib/components/Ide/TestList.svelte';
   import TreeViewer from '$lib/components/Ide/TreeViewer.svelte';
 </script>
 
-<section class="">
-  <Toolbar />
-  <GrammarEditor />
-  <TestList />
-  <TreeViewer />
-  <StatusBar />
+<section class="relative flex h-full w-full flex-col">
+  <div class="flex grow flex-row overflow-auto">
+    <div class="relative w-64 shrink bg-gray-800 text-gray-100">
+      <Projects />
+    </div>
+    <div class="relative grow bg-gray-50">
+      <GrammarEditor />
+    </div>
+    <div class="relative w-1/5 shrink bg-gray-50">
+      <TestList />
+    </div>
+    <div class="relative w-1/5 shrink bg-gray-50">
+      <TreeViewer />
+    </div>
+  </div>
+  <div class="w-full bg-gray-800 text-gray-100">
+    <StatusBar />
+  </div>
 </section>
--- src/lib/components/Ide/Toolbar.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-</script>
-
-<section>
-  <p>This is the brand new IDE Toolbar.</p>
-</section>
