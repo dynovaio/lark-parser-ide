89067568892ffaf575b8584d68a5f4d3db61be68
zodiacfireworks <martin.vuelta@gmail.com>
2025-06-24 04:02:35 -0500

    Update: enhance scrolling behavior in mobile devices

--- src/lib/components/Loader.svelte
+++ src/lib/components/Loader.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import LarkParserLogoSquare from '$lib/components/Icon/LarkParserLogoSquare.svelte';
+  import LarkParserLogo from '$lib/components/Icon/LarkParserLogo.svelte';
   interface Props {
     message: string;
     progress: number;
@@ -9,12 +9,12 @@
 </script>
 
 <div
-  class="page-loader flex h-full w-full flex-col items-center justify-center space-y-4 text-gray-800"
+  class="flex h-screen w-screen flex-col items-center justify-center space-y-4 bg-gray-100 text-gray-800"
 >
-  <LarkParserLogoSquare size={256} aria-label="Lark Parser" />
-  <div class="relative h-6 w-64 overflow-hidden rounded-full bg-black/40">
+  <LarkParserLogo size={142.272} aria-label="Lark Parser" class="w-64" />
+  <div class="relative h-6 w-64 overflow-hidden rounded-full bg-gray-200">
     <div
-      class="h-full w-full bg-green-500 transition-transform duration-500 ease-in-out"
+      class="h-6 w-64 bg-green-500 transition-transform duration-500 ease-in-out"
       style={`transform: translateX(-${100 - (100 * (progress ?? 0)) / 100}%)`}
     ></div>
   </div>
