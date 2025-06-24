994776c4337ec905a9fd10269d0fee55d0d22577
zodiacfireworks <martin.vuelta@gmail.com>
2025-06-24 06:39:25 -0500

    Update: add TestSatus enum

--- src/lib/stores/Ide.ts
+++ src/lib/stores/Ide.ts
@@ -1,3 +1,16 @@
 import { writable } from 'svelte/store';
 
 export const useLegacyIde = writable(false);
+
+export enum TestStatus {
+  UNKNOWN = 'Unknown',
+  SUCCESS = 'Success',
+  FAILURE = 'Failure',
+  PARSING = 'Parsing'
+}
+
+export type TestResult = {
+  status: TestStatus;
+  message?: string;
+  stack?: string;
+};
