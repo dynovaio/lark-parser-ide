05f90f38a53c477bc9e97b9efc11c4dd34b443c1
zodiacfireworks <martin.vuelta@gmail.com>
2025-06-26 03:16:05 -0500

    Update: apply theme to status bar

--- src/lib/components/Ide/StatusBar.svelte
+++ src/lib/components/Ide/StatusBar.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
   import Check from 'phosphor-svelte/lib/CheckCircle';
+  import Code from 'phosphor-svelte/lib/Code';
+  import Error from 'phosphor-svelte/lib/XCircle';
   import Spinner from 'phosphor-svelte/lib/Spinner';
   import Warning from 'phosphor-svelte/lib/WarningCircle';
-  import Error from 'phosphor-svelte/lib/XCircle';
 
-  import { TestStatus } from '$lib/stores/Ide';
+  import { TestStatus } from '$lib/utils/TestCase';
 
   interface Props {
     projectName?: string;
@@ -15,40 +16,78 @@
   let { projectName, testId, testStatus = TestStatus.UNKNOWN }: Props = $props();
 </script>
 
-<div
-  class="relative flex h-8 w-full flex-row justify-between bg-gray-800 px-4 py-2 text-sm text-gray-100"
->
-  <div class="flex shrink-0 flex-row items-center space-x-2">
+<div class="statusbar">
+  <div class="statusbar__section" class:status-warning={!projectName}>
     {#if !projectName}
-      <Warning class="text-yellow-400" />
+      <Warning />
+    {:else}
+      <Code />
     {/if}
     <div>{projectName || 'No project selected'}</div>
   </div>
-  <div class="flex shrink-0 flex-row items-center space-x-2">
+  <div class="statusbar__section" class:status-warning={!testId}>
     {#if testId}
       <span>
         Test {testId}
       </span>
       <span> : </span>
       <span
-        class:text-red-400={testStatus === TestStatus.FAILURE}
-        class:text-green-300={testStatus === TestStatus.SUCCESS}
-        class:text-yellow-300={testStatus === TestStatus.UNKNOWN}
+        class:status-error={testStatus === TestStatus.FAILURE}
+        class:status-success={testStatus === TestStatus.SUCCESS}
+        class:status-warning={testStatus === TestStatus.UNKNOWN}
       >
         {testStatus}
       </span>
-      {#if testStatus === TestStatus.FAILURE}
-        <Error class="text-red-400" />
-      {:else if testStatus === TestStatus.PARSING}
-        <Spinner class="animate-spin" />
-      {:else if testStatus === TestStatus.SUCCESS}
-        <Check class="text-green-300" />
-      {:else if testStatus === TestStatus.UNKNOWN}
-        <Warning class="text-yellow-300" />
-      {/if}
+      <span
+        class:status-error={testStatus === TestStatus.FAILURE}
+        class:status-success={testStatus === TestStatus.SUCCESS}
+        class:status-warning={testStatus === TestStatus.UNKNOWN}
+      >
+        {#if testStatus === TestStatus.FAILURE}
+          <Error />
+        {:else if testStatus === TestStatus.PARSING}
+          <Spinner class="animate-spin" />
+        {:else if testStatus === TestStatus.SUCCESS}
+          <Check />
+        {:else if testStatus === TestStatus.UNKNOWN}
+          <Warning />
+        {/if}
+      </span>
     {:else}
-      <Warning class="text-yellow-300" />
+      <Warning />
       <span>No test selected</span>
     {/if}
   </div>
 </div>
+
+<style lang="postcss">
+  @reference "../../../app.css";
+
+  .statusbar {
+    @apply relative flex h-8 w-full flex-row justify-between px-4 py-2 text-sm;
+    @apply bg-gray-900 text-gray-100;
+    @apply dark:bg-gray-100 dark:text-gray-900;
+  }
+
+  .statusbar__section {
+    @apply flex shrink-0 flex-row items-center gap-x-2;
+  }
+
+  .status-warning,
+  :global(svg).status-warning {
+    @apply text-yellow-300;
+    @apply dark:text-yellow-700;
+  }
+
+  .status-success,
+  :global(svg).status-success {
+    @apply text-green-300;
+    @apply dark:text-green-700;
+  }
+
+  .status-error,
+  :global(svg).status-error {
+    @apply text-red-300;
+    @apply dark:text-red-700;
+  }
+</style>
