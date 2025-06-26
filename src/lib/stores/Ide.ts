c90a4094b4aa4862c4e67fdbe2b60a6ef0a0d83a
zodiacfireworks <martin.vuelta@gmail.com>
2025-06-26 02:18:12 -0500

    Update: simplify ide store

--- src/lib/stores/Ide.ts
+++ src/lib/stores/Ide.ts
@@ -1,39 +1,9 @@
 import { writable } from 'svelte/store';
 
-export const useLegacyIde = writable(false);
-
-export type Grammar = {
-  uri?: string;
-  content?: string;
-};
-
-export enum TestStatus {
-  UNKNOWN = 'Unknown',
-  SUCCESS = 'Success',
-  FAILURE = 'Failure',
-  PARSING = 'Parsing'
-}
-
-export type TestResult = {
-  status: TestStatus;
-  message?: string;
-  stack?: string;
-  content?: string;
-};
-
-export type TestCase = {
-  id: number;
-  description?: string;
-  content?: string;
-  result?: TestResult;
-};
-
-export type Project = {
-  id: string;
-  name: string;
-  grammar: Grammar;
-  testCases: TestCase[];
-};
+import type { Grammar } from '$lib/utils/Grammar';
+import type { TestResult, TestCase } from '$lib/utils/TestCase';
+import type { Project } from '$lib/utils/Project';
+import { PROJECT_TEMPLATE } from '$lib/utils/Project';
 
 export type IdeState = {
   project: Project;
@@ -41,143 +11,7 @@ export type IdeState = {
   testResult?: TestResult;
 };
 
-export const PROJECT_TEMPLATE: Project = {
-  id: 'template',
-  name: 'Template Project',
-  grammar: {
-    uri: 'blank.lark',
-    content: ''
-  },
-  testCases: [
-    {
-      id: 1,
-      description: 'Empty Test Case',
-      content: ''
-    }
-  ]
-};
-
-export const PROJECT_HELLO_WORLD: Project = {
-  id: 'hello-world',
-  name: 'Hello World',
-  grammar: {
-    uri: 'hello-world.lark',
-    content: 'Hello, World!'
-  },
-  testCases: [
-    {
-      id: 1,
-      description: 'Basic Hello World Test',
-      content: 'Hello, World!'
-    }
-  ]
-};
-
-export const PROJECT_JSON: Project = {
-  id: 'json',
-  name: 'JSON',
-  grammar: {
-    uri: 'json.lark',
-    content: '{"this": ["is", "JSON"]}'
-  },
-  testCases: [
-    {
-      id: 1,
-      description: 'Basic JSON Test',
-      content: '{"key": "value"}'
-    }
-  ]
-};
-
-export const PROJECT_CALCULATOR: Project = {
-  id: 'calculator',
-  name: 'Calculator',
-  grammar: {
-    uri: 'calculator.lark',
-    content: '2 + 20 / (13 - 6) + 1.5'
-  },
-  testCases: [
-    {
-      id: 1,
-      description: 'Basic Calculator Test',
-      content: '2 + 20 / (13 - 6) + 1.5'
-    }
-  ]
-};
-
-export const PROJECT_FRUIT_FLIES: Project = {
-  id: 'fruit-flies',
-  name: 'Fruit Flies',
-  grammar: {
-    uri: 'fruit-flies.lark',
-    content: 'fruit flies like bananas'
-  },
-  testCases: [
-    {
-      id: 1,
-      description: 'Basic Fruit Flies Test',
-      content: 'fruit flies like bananas'
-    }
-  ]
-};
-
-export const PROJECT_SEMVER: Project = {
-  id: 'semver',
-  name: 'SemVer',
-  grammar: {
-    uri: 'semver.lark',
-    content: '1.0.0-rc1+build.0001'
-  },
-  testCases: [
-    {
-      id: 1,
-      description: 'Basic SemVer Test',
-      content: '1.0.0-rc1+build.0001'
-    }
-  ]
-};
-
-export const PROJECT_CONFIGURATION: Project = {
-  id: 'configuration',
-  name: 'Configuration',
-  grammar: {
-    uri: 'configuration.lark',
-    content: 'key=value\nanother_key=another_value'
-  },
-  testCases: [
-    {
-      id: 1,
-      description: 'Basic Configuration Test',
-      content: 'key=value\nanother_key=another_value'
-    }
-  ]
-};
-
-export const PROJECT_LARK: Project = {
-  id: 'lark',
-  name: 'Lark',
-  grammar: {
-    uri: 'lark.lark',
-    content: 'start: "Hello" "World"'
-  },
-  testCases: [
-    {
-      id: 1,
-      description: 'Basic Lark Test',
-      content: 'start: "Hello" "World"'
-    }
-  ]
-};
-
-export const SAMPLE_PROJECTS: Project[] = [
-  PROJECT_HELLO_WORLD,
-  PROJECT_JSON,
-  PROJECT_CALCULATOR,
-  PROJECT_FRUIT_FLIES,
-  PROJECT_SEMVER,
-  PROJECT_CONFIGURATION,
-  PROJECT_LARK
-];
+export const useLegacyIde = writable(false);
 
 export const createIdeState = (project?: Project) => {
   if (!project) {
@@ -192,9 +26,43 @@ export const createIdeState = (project?: Project) => {
 
   return {
     subscribe,
-    setProject: (project: Project) =>
-      set({ project, testCase: project.testCases[0], testResult: project.testCases[0]?.result }),
-    setTestCase: (testCase: TestCase) => update((state) => ({ ...state, testCase })),
-    setTestResult: (testResult: TestResult) => update((state) => ({ ...state, testResult }))
+    setProject: (project: Project) => {
+      set({
+        project,
+        testCase: project.testCases[0],
+        testResult: project.testCases[0]?.result
+      });
+    },
+    updateTestCase: (testCase: TestCase) => {
+      update((state) => {
+        return { ...state, testCase };
+      });
+    },
+    updateTestResult: (testResult: TestResult) => {
+      update((state) => {
+        return {
+          ...state,
+          testResult
+        };
+      });
+    },
+    updateGrammar: (grammar: Grammar) => {
+      update((state) => {
+        return {
+          ...state,
+          project: {
+            ...state.project,
+            grammar: {
+              ...state.project.grammar,
+              ...grammar
+            }
+          }
+        };
+      });
+    }
   };
 };
+
+/*
+72 + 32
+*/
