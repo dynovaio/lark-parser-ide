132816213a5ea5290fa1c6721e1d1548d0d7f9d9
zodiacfireworks <martin.vuelta@gmail.com>
2025-06-26 01:00:45 -0500

    Update: apply dark theme variant to loader

--- src/lib/components/Loader.svelte
+++ src/lib/components/Loader.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import LarkParserLogo from '$lib/components/Icon/LarkParserLogo.svelte';
+
   interface Props {
     message: string;
     progress: number;
@@ -8,15 +9,45 @@
   const { message = $bindable('Loading...'), progress = $bindable(0) }: Props = $props();
 </script>
 
-<div
-  class="flex h-screen w-screen flex-col items-center justify-center space-y-4 bg-gray-100 text-gray-800"
->
-  <LarkParserLogo size={142.272} aria-label="Lark Parser" class="w-64" />
-  <div class="relative h-6 w-64 overflow-hidden rounded-full bg-gray-200">
-    <div
-      class="h-6 w-64 bg-green-500 transition-transform duration-500 ease-in-out"
-      style={`transform: translateX(-${100 - (100 * (progress ?? 0)) / 100}%)`}
-    ></div>
+<div class="loader">
+  <LarkParserLogo size={142.272} aria-label="Lark Parser" />
+  <div
+    class="progressbar"
+    role="progressbar"
+    aria-valuenow={progress}
+    aria-valuemin="0"
+    aria-valuemax="100"
+  >
+    <div class="progressbar__progress" data-progress={progress}></div>
   </div>
-  <p class="">{message}</p>
+  <p class="message">{message}</p>
 </div>
+
+<style lang="postcss">
+  @reference "../../app.css";
+
+  .loader {
+    @apply flex h-screen w-screen flex-col items-center justify-center space-y-4;
+    @apply bg-gray-100 text-gray-900;
+    @apply dark:bg-gray-900 dark:text-gray-100;
+  }
+
+  .progressbar {
+    @apply relative h-6 w-64 overflow-hidden rounded-full;
+    @apply bg-gray-200;
+    @apply bg-gray-200;
+  }
+
+  .progressbar__progress {
+    --progress: attr(data-progress %, 0);
+
+    @apply h-6 w-64 transition-transform duration-500 ease-in-out;
+    @apply bg-green-500;
+
+    transform: translateX(calc(var(--progress) - 100%));
+  }
+
+  .message {
+    @apply text-center;
+  }
+</style>
