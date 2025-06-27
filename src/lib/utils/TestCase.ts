e920dbdfbd67c13029ac1052691dbd51d75b5ca4
zodiacfireworks <martin.vuelta@gmail.com>
2025-06-27 15:24:04 -0500

    Update: new executeTestCase function

--- src/lib/utils/TestCase.ts
+++ src/lib/utils/TestCase.ts
@@ -1,3 +1,7 @@
+import { toPythonCompatibleParserOptions, type ParserOptions } from '$lib/utils/Parser';
+import { type PyodideModule } from '$lib/utils/Pyodide';
+import { type Grammar } from '$lib/utils/Grammar';
+
 export enum TestStatus {
   UNKNOWN = 'Unknown',
   SUCCESS = 'Success',
@@ -31,4 +35,54 @@ export const TEST_CASE_TEMPLATE: TestCase = {
   description: 'New Test Case',
   content: '',
   result: { ...TEST_CASE_RESULT_TEMPLATE }
+};
+
+const DEFINE_EXECUTE_TEST_CASE_SCRIPT = `
+def execute_test_case(grammar, test_string, options):
+    result = { "status": "Unknown" }
+
+    try:
+        parser = lark.Lark(
+          grammar,
+          **options,
+        )
+
+        result = json.dumps(
+          parser.parse(test_string),
+          cls=LarkEncoder,
+          indent=2,
+        )
+
+        result = {
+            "status": "Success",
+            "content": result
+        }
+    except Exception as e:
+        result = {
+            "status": "Failure",
+            "message": str(e).strip(),
+            "traceback": traceback.format_exc().strip(),
+            "content": ""
+        }
+
+    return json.dumps(result)
+`;
+
+export const executeTestCase = async (
+  executor: PyodideModule,
+  parserOptions: ParserOptions,
+  grammar: Grammar,
+  testCase: TestCase
+): Promise<TestResult> => {
+  const pythonCompatibleParserOptions = toPythonCompatibleParserOptions(parserOptions);
+
+  executor.globals.set('parser_options', pythonCompatibleParserOptions);
+  executor.globals.set('grammar', grammar.content || '');
+  executor.globals.set('test_string', testCase.content || '');
+  await executor.runPythonAsync(DEFINE_EXECUTE_TEST_CASE_SCRIPT);
+  const result = await executor.runPythonAsync(
+    `execute_test_case(grammar, test_string, parser_options.to_py())`
+  );
+
+  return JSON.parse(result) as TestResult;
 };
